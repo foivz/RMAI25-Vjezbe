760e74699ce4eb747fbb5c0b8935be272cf907a4
Dario Ljubas <darioljubas33@gmail.com>
2026-03-13 09:12:10 +0100

    Kreiran projekt; Implementirana navigacija kroz TabLayout

---
+++ README.md
@@ -0,0 +1,1 @@
+# RMAI25-Vjezbe
---
+++ app/src/main/java/hr/foi/rmai/memento/adapters/MainPagerAdapter.kt
@@ -0,0 +1,57 @@
+package hr.foi.rmai.memento.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import hr.foi.rmai.memento.R
+import hr.foi.rmai.memento.fragments.CompletedFragment
+import hr.foi.rmai.memento.fragments.NewsFragment
+import hr.foi.rmai.memento.fragments.PendingFragment
+import kotlin.reflect.KClass
+
+class MainPagerAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle){
+
+    data class FragmentItem(
+        val titleRes: Int,
+        val iconRes: Int,
+        val fragmentClass: KClass<*>
+    )
+
+    val fragmentItems = listOf(
+        FragmentItem(
+            R.string.tasks_pending,
+            R.drawable.outline_assignment_late_24,
+            PendingFragment::class
+        ),
+        FragmentItem(
+            R.string.tasks_completed,
+            R.drawable.outline_assignment_turned_in_24,
+            CompletedFragment::class
+        ),
+        FragmentItem(
+            R.string.news,
+            R.drawable.outline_breaking_news_24,
+            NewsFragment::class
+        )
+    )
+
+    override fun getItemCount(): Int {
+        return fragmentItems.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentItems[position]
+                .fragmentClass
+                .java.getDeclaredConstructor()
+                .newInstance() as Fragment
+    }
+}
+
+
+
+
+
